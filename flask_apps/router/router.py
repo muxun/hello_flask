1f46a46b130fe500db50f28f4e06b0a3c67eabe4
muxun <muxund@gmail.com>
2019-02-25 17:27:37 +0300

    Adding working files

---
+++ flask_apps/router/router.py
@@ -0,0 +1,17 @@
+import os
+from flask import Flask, redirect
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "add /hello after ip:port"
+
+@app.route("/hello")
+def hello():
+    return redirect("http://ip:5000")
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0')
+
